4fd7ab4fe1ca2aa562b5dcda7ba5856fa250883e
fashion-challenge <38303695+fashion-challenge@users.noreply.github.com>
2018-04-13 16:14:18 +0800

    upload initial files

---
+++ src/js/ai-competition.js
@@ -0,0 +1,7 @@
+$(function() {
+    $('#content_container').fullpage({
+        sectionsColor: ['#fff', '#fff', '#fff', '#fff', '#f7f7f7', '#fff'],
+        anchors: ['index', 'introduction', 'fashion-style', 'fashion-item', 'prize', 'comp-date'],
+        menu: '#menu'
+    });
+});
